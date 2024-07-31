97ca095263a6168f330d43b5c766c671ed182db5
SmallChX <minhthuan55588@gmail.com>
2024-08-01 00:49:13 +0700

    CQB-20 add pkg jwt

---
+++ server/pkg/jwt/jwt.go
@@ -0,0 +1,62 @@
+package jwt
+
+import (
+	"errors"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var (
+	ErrInvalidToken = errors.New("invalid token")
+)
+
+type UserClaims struct {
+	jwt.StandardClaims
+	UserID string
+	Role   string
+}
+
+func Generate(userID, role string) (string, error) {
+	jwtSecretKey := os.Getenv("JWT_SECRET_KEY")
+	tokenDuration, _ := strconv.Atoi((os.Getenv("TOKEN_DURATION")))
+	claims := UserClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Duration(tokenDuration)).Unix(),
+		},
+		UserID: userID,
+		Role:   role,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	tokenString, err := token.SignedString(jwtSecretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func Verify(accessToken string) (*UserClaims, error) {
+	jwtSecretKey := os.Getenv("JWT_SECRET_KEY")
+
+	token, err := jwt.ParseWithClaims(
+		accessToken,
+		&UserClaims{},
+		func(t *jwt.Token) (interface{}, error) {
+			return []byte(jwtSecretKey), nil
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*UserClaims)
+	if !ok || !token.Valid {
+		return nil, ErrInvalidToken
+	}
+
+	return claims, nil
+}

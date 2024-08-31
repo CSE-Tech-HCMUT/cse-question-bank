da584993583b4d5d847d92919310d0c217dfc125
SmallChX <minhthuan55588@gmail.com>
2024-09-01 01:27:13 +0700

    CQB-27 create factory

---
+++ server/internal/module/question/usecase/fill_in_blank_usecase.go
@@ -0,0 +1,5 @@
+package usecase
+
+type fillInBlankUsecaseImpl struct {
+	questionBaseUsecaseImpl
+}
---
+++ server/internal/module/question/usecase/question_usecase_interface.go
@@ -0,0 +1,8 @@
+package usecase
+
+type QuestionUsecase interface {
+	GetQuestion()
+	UpdateQuestion()
+	CreateQuestion()
+	DeleteQuestion()
+}

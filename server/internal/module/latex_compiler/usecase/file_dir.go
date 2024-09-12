50f603d0760f3c3ac287abea6eb489b245e362e4
SmallChX <minhthuan55588@gmail.com>
2024-09-13 01:13:17 +0700

    CQB-28 enhance latex compile

--- server/internal/module/latex_compiler/usecase/file_dir.go
+++ server/internal/module/latex_compiler/usecase/file_dir.go
@@ -1,75 +1,67 @@
 package usecase
 
 import (
+	"cse-question-bank/internal/module/latex_compiler/model"
+	"encoding/json"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/google/uuid"
 )
 
-func createFolder() (string, error) {
+const path string = "./internal/module/latex_compiler/latex_source/"
+
+func (u *latexCompilerImpl) createFolder() (string, error) {
 	folderName := uuid.New().String()
-	folderPath := "./internal/module/latex_compiler/latex_source/" + folderName
+	folderPath := path + folderName
 	err := os.Mkdir(folderPath, os.ModeAppend)
-	if err != nil {
-		return "", err
-	}
-	// có 2 vấn đề:
-	// 		1. 1 số package chưa có phải tải -> lỗi (lỗi timeout), phải config để package đc tải on the fly
-	// 		2. Ban đầu khi t dùng package minted thì nó lỗi là thiếu outdir cho cài package này, phải chỉnh lại thêm
-	// 		[cache=false] thì mới chạy đc
-
-	packageContent := `\documentclass[preview,border=12pt]{standalone}
-\usepackage{amsmath} % Optional, for math environments if needed
-\usepackage{geometry}
-\usepackage{vntex}
-
-
-% Set page geometry to increase width
-\geometry{a4paper, margin=2cm}`
-
-	mintedPackage := `\usepackage[outputdir=` + folderPath + `]{minted}`
-
-	docContent := `
-\begin{document}
-% Your question here
-\textbf{Câu 2:} Cho biết giá trị các phần tử của mảng A sau khi thực hiện đoạn lệnh dưới đây:
-
-\begin{minted}[xleftmargin=30pt,linenos,breaklines,breakanywhere]{cpp}
-int A[] = ...;
-int b, *p=A+2;
-b =* p++;
-*p+=2*b;
-p++;
-*p+=1;
-\end{minted}
-
-\begin{enumerate}
-\item {5, 7, 20, 20, 2, 6}
-\item {6, 20, 19, 1, 3, 6}
-\item {5, 7, 19, 1, 2, 6}
-\item \textbf{{5, 7, 19, 39, 3, 6}}
-\end{enumerate}
-\end{document}`
-
-	texContent := packageContent + "\n" + mintedPackage + "\n" + docContent
-
-	// Tên file .tex
-	fileName := folderPath + "/output.tex"
-
-	// Tạo và mở file .tex
-	file, err := os.Create(fileName)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	// Ghi nội dung vào file .tex
-	_, err = file.WriteString(texContent)
 	if err != nil {
 		return "", err
 	}
 
 	return folderPath, nil
+}
+
+func (u *latexCompilerImpl) createOuputLatexFile(folderPath string, question *model.QuestionCompile) error {
+	packageFile, err := os.ReadFile(path + "package.tex")
+	if err != nil {
+		return err
+	}
+	packageContent := string(packageFile)
+
+	contentTemplateFile, err := os.ReadFile(path + "content_template.tex")
+	if err != nil {
+		return err
+	}
+
+	contentTemplate := string(contentTemplateFile)
+
+	questionContent, err := u.GenerateQuestionContent(question)
+	if err != nil {
+		return err
+	}
+
+	contentTemplate = strings.Replace(contentTemplate, "<<QUESTION_CONTENT>>", questionContent, -1)
+
+	finalContent := packageContent + "\n" + contentTemplate
+
+	// Tên file .tex
+	fileName := folderPath + "/output.tex"
+	print(fileName)
+	// Tạo và mở file .tex
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Ghi nội dung vào file .tex
+	_, err = file.WriteString(finalContent)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func deleteFolder(folderPath string) error {
@@ -79,4 +71,60 @@ func deleteFolder(folderPath string) error {
 	}
 
 	return nil
+}
+
+type MultipleChoiceAnswer struct {
+	Content   string
+	IsCorrect bool `json:"is-correct"`
+}
+
+func (u *latexCompilerImpl) GenerateQuestionContent(question *model.QuestionCompile) (string, error) {
+	var result string
+	if question.IsParent {
+		subQuestionsCount := len(question.SubQuestions)
+
+		result = fmt.Sprintf("\\begin{block}[questions=%d]\nĐoạn mô tả sau được áp dụng cho các câu \\thefirst-\\thelast.\n\n%s", subQuestionsCount, question.Content)
+		for _, subQuestion := range question.SubQuestions {
+			subQuestionContent, err := u.GenerateQuestionContent(subQuestion)
+			if err != nil {
+				return "", err
+			}
+			result += subQuestionContent + "\n"
+		}
+
+		result += "\\end{block}"
+	} else {
+		result = "\\begin{question}\n" + question.Content + "\n"
+
+		answerContent, err := u.GenerateAnswerContent(question.Answer)
+		if err != nil {
+			return "", err
+		}
+		result += answerContent + "\n\\end{question}"
+	}
+	
+	return result, nil
+}
+
+func (u *latexCompilerImpl) GenerateAnswerContent(answerContent json.RawMessage) (string, error) {
+	var answers []MultipleChoiceAnswer
+
+	err := json.Unmarshal(answerContent, &answers)
+	if err != nil {
+		return "", err
+	}
+
+	result := "\\datcot\n\\bonpa\n"
+
+	for _, answer := range answers {
+		var correctString string
+		if answer.IsCorrect {
+			correctString = "dung"
+		} else {
+			correctString = "sai"
+		}
+		result += fmt.Sprintf("{\\%s{%s}}\n", correctString, answer.Content)
+	}
+
+	return result, nil
 }

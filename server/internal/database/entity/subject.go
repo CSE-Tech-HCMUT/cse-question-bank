4b54c8cfd7c8ecbf7e866595cc2e0315d7227dd0
Thuan Tran <thuan.tran@opswat.com>
2024-12-24 16:30:42 +0700

    minor fix'

--- server/internal/database/entity/subject.go
+++ server/internal/database/entity/subject.go
@@ -1,14 +1,26 @@
 package entity
 
-import "github.com/google/uuid"
+import (
+	"cse-question-bank/internal/util"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Subject struct {
-	Id             uuid.UUID	`gorm:"type:uuid;primaryKey"`
+	Id             uuid.UUID `gorm:"type:uuid;primaryKey"`
 	Name           string
-	Code           string	`gorm:"uniqueIndex"`
+	Code           string `gorm:"uniqueIndex"`
 	DepartmentCode string
 	Department     Department `gorm:"foreignKey:DepartmentCode"`
 	Users          []User     `gorm:"many2many:subject_users"`
 	Questions      []Question `gorm:"foreignKey:SubjectId;constraint:OnDelete:CASCADE"`
 	Tags           []Tag      `gorm:"foreignKey:SubjectId;constraint:OnDelete:CASCADE"`
+}
+
+func (s *Subject) BeforeCreate(tx *gorm.DB) (err error) {
+	if s.Id == uuid.Nil {
+		s.Id, err = util.GenerateUUID()
+	}
+	return
 }
